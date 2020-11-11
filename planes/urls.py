87fd259fab7e0d8532d294364f701dac2b61b62f
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-11-11 11:00:50 -0300

    arreglar el error del jorge

--- planes/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.plan_list, name='plan_list'),
-]
--- planes/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Plan
-from django.utils import timezone
-
-# Create your views here.
-
-def plan_list(request):
-    planes = Plan.objects.all()
-    return render(request, 'planes/plan_list.html', {'planes': planes})
