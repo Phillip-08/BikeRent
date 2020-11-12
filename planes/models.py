77e4fe7f21961ae30f23a41b0562a6a3216e334c
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-11-12 00:06:03 -0300

    Agregar imagenes a la BD

--- planes/models.py
+++ planes/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
 
+fs = FileSystemStorage(location='/media/photos')
 class Plan(models.Model):
     NombrePlan = models.CharField(max_length=30)
     descripcion = models.TextField();
     precio = models.IntegerField();
     duracion = models.DateTimeField(default=timezone.now);
+    imagen = models.ImageField(upload_to="planes", null = True)
     
     def __str__(self):
         return self.NombrePlan
