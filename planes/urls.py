9fd6ca8a68ce25e38e1772c0e6996706f041ec3a
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-11-12 20:09:06 -0300

    Agregar plan_venta

--- planes/urls.py
+++ planes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.plan_list, name='plan_list'),
+    path('plan/<int:pk>/', views.plan_venta, name='plan_venta'),
 ]
--- planes/views.py
+++ planes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render, get_object_or_404
 from .models import Plan
 from django.utils import timezone
 
@@ -7,3 +8,7 @@ from django.utils import timezone
 def plan_list(request):
     planes = Plan.objects.all()
     return render(request, 'planes/plan_list.html', {'planes': planes})
+
+def plan_venta(request, pk):
+    plan = get_object_or_404(Plan, pk=pk)
+    return render(request, 'planes/plan_venta.html', {'plan' : plan}) 
