dcdcc20dc019fe6abc5e82edaa0fa1ca8b43cd4f
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-11-15 23:43:33 -0300

    Subir python

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('planes.urls')),
+    path('', RedirectView.as_view(url='planes/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
