027fbc0f17c82c66e44da6c9fc5e0d64d999f93f
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-12-11 22:20:12 -0300

    se agregan mas cosas

--- planes/serializers.py
+++ planes/serializers.py
@@ -1,8 +1,9 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Plan
 
 class Planserielizers (serializers.ModelSerializer):
-
     class Meta:
         model = Plan
-        fields = ["NombrePlan","descripcion","precio","fecha"]
+        fields = '__all__'
+        #fields = ["NombrePlan","descripcion","precio","fecha"]
